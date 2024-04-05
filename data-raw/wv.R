9d77da2d2d2f28d4abb5ceb78a9404d0a1c9ee1f
Christopher Kenny <ctk44@cornell.edu>
2024-04-05 14:08:31 -0500

    update plans to be equal size per chain

--- data-raw/wv.R
+++ data-raw/wv.R
@@ -2,10 +2,18 @@ wv <- alarmdata::alarm_50state_map('wv')
 pl <- alarmdata::alarm_50state_plans('wv')
 
 set.seed(censable::match_fips('WV'))
-dr <- sample(redist::subset_sampled(pl)$draw, size = 10)
+dr <- pl |>
+  subset_sampled() |>
+  as_tibble() |>
+  group_by(chain) |>
+  slice_sample(n = 3) |>
+  ungroup() |>
+  pull(draw) |>
+  as.double() |>
+  sort()
 
 pl_sub <- redist::get_plans_matrix(redist::filter(pl, draw %in% dr))
-colnames(pl_sub) <- paste0('sample_', 1:10)
+colnames(pl_sub) <- paste0('sample_', 1:12)
 
 wv <- dplyr::bind_cols(wv, dplyr::as_tibble(pl_sub))
 class(wv) <- c(class(wv)[1], 'sf', class(wv)[-1])
